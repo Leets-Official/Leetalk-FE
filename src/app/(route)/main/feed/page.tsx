afa1445eda3be0f1b688f08593037fe1cf18a314
One-HyeWon <5876675@naver.com>
2024-03-02 04:01:10 +0900

    :wrench:[recycle]: main feed 파일 경로 수정

--- src/app/(route)/main/feed/page.tsx
+++ src/app/(route)/main/feed/page.tsx
@@ -1,7 +1,7 @@
-import Footer from '../../components/layout/Footer';
-import Header from '../../components/layout/Header';
-import CategoriesContainer from '../../components/container/CategoriesContainer';
-import FeedItemContainer from '../../components/main/container/FeedItemContainer';
+import Footer from '../../../components/layout/Footer';
+import Header from '../../../components/layout/Header';
+import CategoriesContainer from '../../../components/container/CategoriesContainer';
+import FeedItemContainer from '../../../components/main/container/FeedItemContainer';
 
 export default function MainPage() {
   return (
