c4f397ce7fa382eab2ec54cc2b5031c7f4df6040
One-HyeWon <5876675@naver.com>
2024-03-10 15:22:18 +0900

    :sparkles:[feat]: 주소 복사

--- src/app/components/articles/Share.tsx
+++ src/app/components/articles/Share.tsx
@@ -1,12 +1,30 @@
+'use client';
+
 import { share } from '../../ui/IconPath';
 import Icons from '../common/Icons';
 
 const Share = () => {
+  const doCopy = () => {
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(window && window.location.href)
+        .then(() => {
+          alert('클립보드에 복사되었습니다.');
+        })
+        .catch(() => {
+          alert('복사를 다시 시도해주세요.');
+        });
+    }
+  };
+
   return (
-    <div>
-      <Icons name={share} />
+    <div className="w-[20%] h-[20%]">
+      <Icons
+        name={share}
+        className=" cursor-pointer"
+        onClick={() => doCopy()}
+      />
     </div>
   );
 };
-
 export default Share;
